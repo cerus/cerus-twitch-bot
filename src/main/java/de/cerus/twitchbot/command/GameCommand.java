fd51eab3b86ed8ddc4ccbe32afa6d61c88cf9d2b
Maximilian Dorn <zocks1703@web.de>
2020-09-10 16:26:00 +0200

    Fix title and game command

--- src/main/java/de/cerus/twitchbot/command/GameCommand.java
+++ src/main/java/de/cerus/twitchbot/command/GameCommand.java
@@ -3,10 +3,11 @@ package de.cerus.twitchbot.command;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
 import com.github.twitch4j.common.events.domain.EventUser;
-import com.github.twitch4j.helix.domain.GameList;
-import com.github.twitch4j.helix.domain.StreamList;
+import com.github.twitch4j.helix.domain.ChannelInformation;
+import com.github.twitch4j.helix.domain.Game;
 import de.cerus.twitchbot.Constants;
 import java.util.Collections;
+import java.util.List;
 
 public class GameCommand extends Command {
 
@@ -16,19 +17,30 @@ public class GameCommand extends Command {
 
     @Override
     public void invoke(final EventUser user, final String[] args, final IRCMessageEvent event) {
-        if (args.length > 0 && Constants.WHITELIST.contains(user.getName())) {
-            //TODO: Change game
+        if (args.length == 0 || !Constants.WHITELIST.contains(user.getName())) {
+            final ChannelInformation channelInfo = this.getTwitchClient().getHelix().getChannelInformation(System.getenv("TWITCH_OAUTH"),
+                    Collections.singletonList(Constants.USER_ID)).execute().getChannels().get(0);
+
+            this.getTwitchClient().getChat().sendMessage(event.getChannel().getName(), "Es wird gerade " + channelInfo.getGameName() + " gespielt.");
             return;
         }
 
-        final StreamList streamList = this.getTwitchClient().getHelix().getStreams(System.getenv("TWITCH_OAUTH"), null, null, 1,
-                null, null, null, Collections.singletonList(Constants.USER_ID), null).execute();
-        final String gameId = streamList.getStreams().get(0).getGameId();
+        final List<Game> games = this.getTwitchClient().getHelix().getGames(System.getenv("TWITCH_OAUTH"), null,
+                Collections.singletonList(String.join(" ", args))).execute().getGames();
 
-        final GameList gameList = this.getTwitchClient().getHelix().getGames(System.getenv("TWITCH_OAUTH"), Collections.singletonList(gameId), null).execute();
-        final String name = gameList.getGames().get(0).getName();
+        if (games.isEmpty()) {
+            return;
+        }
+        final Game game = games.get(0);
 
-        this.getTwitchClient().getChat().sendMessage(event.getChannel().getName(), "Es wird gerade " + name + " gespielt.");
+        final ChannelInformation channelInfo = this.getTwitchClient().getHelix().getChannelInformation(System.getenv("CERUS_TOKEN"),
+                Collections.singletonList(Constants.USER_ID)).execute().getChannels().get(0);
+        final ChannelInformation newChannelInfo = new ChannelInformation(channelInfo.getBroadcasterId(), channelInfo.getBroadcasterName(),
+                channelInfo.getBroadcasterLanguage(), game.getId(), game.getName(), channelInfo.getTitle());
+
+        this.getTwitchClient().getHelix().updateChannelInformation(System.getenv("CERUS_TOKEN"), Constants.USER_ID, newChannelInfo).execute();
+
+        this.getTwitchClient().getChat().sendMessage(event.getChannel().getName(), "Es wird jetzt " + game.getName() + " gespielt.");
     }
 
 }
--- src/main/java/de/cerus/twitchbot/command/TitleCommand.java
+++ src/main/java/de/cerus/twitchbot/command/TitleCommand.java
@@ -3,7 +3,9 @@ package de.cerus.twitchbot.command;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
 import com.github.twitch4j.common.events.domain.EventUser;
+import com.github.twitch4j.helix.domain.ChannelInformation;
 import de.cerus.twitchbot.Constants;
+import java.util.Collections;
 
 public class TitleCommand extends Command {
 
@@ -13,15 +15,17 @@ public class TitleCommand extends Command {
 
     @Override
     public void invoke(final EventUser user, final String[] args, final IRCMessageEvent event) {
-        if (!Constants.WHITELIST.contains(user.getName())) {
+        if (!Constants.WHITELIST.contains(user.getName()) || args.length == 0) {
             return;
         }
 
-        if (args.length == 0) {
-            return;
-        }
+        final ChannelInformation channelInfo = this.getTwitchClient().getHelix().getChannelInformation(System.getenv("CERUS_TOKEN"),
+                Collections.singletonList(Constants.USER_ID)).execute().getChannels().get(0);
+        final ChannelInformation newChannelInfo = new ChannelInformation(channelInfo.getBroadcasterId(), channelInfo.getBroadcasterName(),
+                channelInfo.getBroadcasterLanguage(), channelInfo.getGameId(), channelInfo.getGameName(), String.join(" ", args));
 
-        this.getTwitchClient().getKraken().updateTitle(System.getenv("TWITCH_OAUTH"), Constants.USER_ID, String.join(" ", args)).execute();
+        this.getTwitchClient().getHelix().updateChannelInformation(System.getenv("CERUS_TOKEN"), Constants.USER_ID, newChannelInfo).execute();
+
         this.getTwitchClient().getChat().sendMessage("realcerus", "Titel wurde geändert");
     }
 
